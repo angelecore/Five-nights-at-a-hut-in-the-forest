96c521dd62f3b80417ee7bbb91c30d99d12e3a23
angelecore <93863610+angelecore@users.noreply.github.com>
2022-03-01 00:10:09 +0200

    Velocity
    added velocity fix

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -10,9 +10,20 @@ public class playerMovement : MonoBehaviour
 
     Vector3 velocity;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
     // Update is called once per frame
     void Update()
-    {
+    {   
+        isGrounded=Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
