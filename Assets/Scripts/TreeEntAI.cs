af54317ceb59f0663a9bb97ee532428dadfd2a1d
Ben10-boop <78535591+Ben10-boop@users.noreply.github.com>
2022-03-27 14:03:24 +0300

    TreeEnt now remembers the parent object
    Accessing parent object should allow to get what is needed from it in order to break It's boards

--- Assets/Scripts/TreeEntAI.cs
+++ Assets/Scripts/TreeEntAI.cs
@@ -11,7 +11,10 @@ public class TreeEntAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = GetTarget();
+        //target - the child object for AI to move to
+        //targetBoardSpawner - parent object, should contain whatever is needed
+        GameObject targetBoardSpawner;
+        target = GetTarget(out targetBoardSpawner);
         agent = GetComponent<NavMeshAgent>();
     }
 
@@ -28,44 +31,60 @@ public class TreeEntAI : MonoBehaviour
         }
     }
 
-    private Transform GetTarget()
+    private Transform GetTarget(out GameObject targetBoardSpawner)
     {
         Transform target = BoardSpawnerTracker.instance.boardSpawnerTarget.transform;
         float distToTarget = Vector3.Distance(target.position, transform.position);
+        targetBoardSpawner = BoardSpawnerTracker.instance.boardSpawner;
+
         Transform target1 = BoardSpawnerTracker.instance.boardSpawnerTarget1.transform;
         float distToTarget1 = Vector3.Distance(target1.position, transform.position);
+        GameObject tBS1 = BoardSpawnerTracker.instance.boardSpawner1;
+
         Transform target2 = BoardSpawnerTracker.instance.boardSpawnerTarget2.transform;
         float distToTarget2 = Vector3.Distance(target2.position, transform.position);
+        GameObject tBS2 = BoardSpawnerTracker.instance.boardSpawner2;
+
         Transform target3 = BoardSpawnerTracker.instance.boardSpawnerTarget3.transform;
         float distToTarget3 = Vector3.Distance(target3.position, transform.position);
+        GameObject tBS3 = BoardSpawnerTracker.instance.boardSpawner3;
+
         Transform target4 = BoardSpawnerTracker.instance.boardSpawnerTarget4.transform;
         float distToTarget4 = Vector3.Distance(target4.position, transform.position);
+        GameObject tBS4 = BoardSpawnerTracker.instance.boardSpawner4;
+
         Transform target5 = BoardSpawnerTracker.instance.boardSpawnerTarget5.transform;
         float distToTarget5 = Vector3.Distance(target5.position, transform.position);
+        GameObject tBS5 = BoardSpawnerTracker.instance.boardSpawner5;
 
         if (distToTarget1 < distToTarget)
         {
             target = target1;
             distToTarget = distToTarget1;
+            targetBoardSpawner = tBS1;
         }
         if (distToTarget2 < distToTarget)
         {
             target = target2;
             distToTarget = distToTarget2;
+            targetBoardSpawner = tBS2;
         }
         if (distToTarget3 < distToTarget)
         {
             target = target3;
             distToTarget = distToTarget3;
+            targetBoardSpawner = tBS3;
         }
         if (distToTarget4 < distToTarget)
         {
             target = target4;
             distToTarget = distToTarget4;
+            targetBoardSpawner = tBS4;
         }
         if (distToTarget5 < distToTarget)
         {
             target = target5;
+            targetBoardSpawner = tBS5;
         }
         return target;
     }
