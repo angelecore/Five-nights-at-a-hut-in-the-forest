93f79ef84e384ddd108557cc160076e5ddb7d4b7
Akechi <sarken12@gmail.com>
2022-03-30 13:31:12 +0300

    barricade animation

--- Assets/Scripts/barricadeScript.cs
+++ Assets/Scripts/barricadeScript.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class barricadeScript : MonoBehaviour
 {
-    public int boards;
+    public int boards,prev;
 
-    
+    public Animator[] boardAnimation;
     public GameObject[] board;
     public Player player;
     // Start is called before the first frame update
     void Start()
     {
-        // player = GetComponent<Player>();
+        boardAnimation = GetComponentsInChildren<Animator>();
+        board[0].SetActive(false);
+        board[1].SetActive(false);
+        board[2].SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(boards != prev)
+        {
+            prev = boards;
+            switch (boards)
+            {
+                case 1:
+                    boardAnimation[0].Play("board1Animation");
+                    return;
+                case 2:
+                    boardAnimation[1].Play("board2Animation");
+                    return;
+                case 3:
+                    boardAnimation[2].Play("board3Animation");
+                    return;
+            }
+        }    
     }
     void addBarricade()
     {
---
+++ Assets/Scripts/playAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAnimation : MonoBehaviour { 
+    public bool play;
+    ParticleSystem[] dust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dust = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play)
+        {
+            dust[0].Play();
+            dust[1].Play();
+            play = false;
+        }
+    }
+}
